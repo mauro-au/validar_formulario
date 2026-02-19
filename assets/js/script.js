249193c0594aa438211c6083a2f4a66af734e3dd
mauro-au <mauricio.a.developer@gmail.com>
2026-02-19 00:24:24 -0300

    Se agrega validación en el formulario — Validación en tiempo real y por submit — Se muestran los errores visualmente con cambio de color y texto descriptivo — además, se deja la información del formulario en un modal al hacer click en submint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,64 @@
-function validar(){
-    var nombre
-    nombre = document.getElementById("nombre").value;
 
-    if (nombre === ""){
-        alert("Todos los campos son obligatorios");
-        return false;
-    }
-    else if (nombre.length>30){
-        alert("El nombre es muy largo");
-        return false;
-    }
+const form = document.querySelector('fieldset');
 
-    alert(saludo);
+
+const createSmall = (parent, errorText) => {
+    const newSmall = document.createElement('small')
+    newSmall.textContent = errorText
+    parent.appendChild(newSmall)
 }
+
+
+const stringInput = (e) => {
+    const parent = e.target.parentElement
+    const small = parent.querySelector('small')
+
+    const regex = /^[a-zA-Z\s]+$/;
+
+    if (e.target.type === 'text') {
+        if (!small) {
+            createSmall(parent, 'El formato de texto es incorrecto');
+        }
+    }
+}
+const telInput = (e) => {
+    const parent = e.target.parentElement
+    const small = parent.querySelector('small')
+    // console.log(typeof e.target.value)
+    const regexChile = /^\+56 9 \d{8}$/;
+    // console.log(regexChile.test(e.target.value))
+    if (e.target.type === 'tel' && e.target.value === '') {
+        if (!small) {
+            createSmall(parent, 'El campo no puede estar vacío')
+        }
+
+    } else if (e.target.type === 'tel' && !regexChile.test(e.target.value)) {
+        if (!small) {
+            createSmall(parent, 'El formato de teléfono es incorrecto')
+        }
+    } else {
+        if (small) small.remove();
+    }
+}
+
+
+const targetInput = (e) => {
+    const parent = e.target.parentElement
+    const small = parent.querySelector('small')
+    // console.log(e.target.attributes.type)
+
+    if (e.target.value === '') {
+        e.target.classList.add('input-error')
+        if (!small) {
+            createSmall(parent, 'El campo no puede estar vacío')
+        }
+    } else {
+        e.target.classList.remove('input-error')
+        if (small) small.remove()
+    }
+
+    stringInput(e);
+    telInput(e);
+}
+
+form.addEventListener('input', targetInput)
---
+++ assets/js/script2.js
@@ -0,0 +1,111 @@
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const userName = document.querySelector("#userName");
+const password = document.querySelector("#password");
+const checkbox = document.querySelector("#checkbox");
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.closeModal');
+const modalContent = document.querySelector('.modalContent');
+
+
+const clearInputs = () => {
+  name.value = ''
+  email.value = ''
+  phone.value = ''
+  userName.value = ''
+  password.value = ''
+  checkbox.checked = false
+}
+
+const getValue = () => {
+  let welcome = document.createElement("div");
+  welcome.innerHTML = `
+    <p>${name.value}</p>
+    <p>${email.value}</p>
+    <p>${phone.value}</p>
+    <p>${userName.value}</p>
+    <p>${password.value}</p>
+    <p>${checkbox.checked ? "Acepta los términos" : ""}</p>
+  `;
+
+  modalContent.appendChild(welcome);
+  modal.showModal();
+};
+
+const createError = (parent, errorText) => {
+  const newSmall = document.createElement("small");
+  newSmall.textContent = errorText;
+  parent.appendChild(newSmall);
+};
+
+const validateEmail = (email) => {
+  const regex = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};
+
+const validatePhone = (phone) => {
+  const regex = /^\+56 9 \d{8}$/;
+  return regex.test(phone);
+};
+
+
+const validator = (input, validate, textError = "El campo no puede estar vacío") => {
+  const parent = input.parentElement;
+  const small = parent.querySelector("small");
+
+  if (validate) {
+    input.classList.add("input-error");
+    if (!small) {
+      return createError(parent, textError);
+    } else {
+      return small.textContent = textError;
+    }
+  }
+
+  input.classList.remove("input-error");
+  if (small) small.remove();
+};
+
+
+// Agregar validación en tiempo real a cada campo
+name.addEventListener("input", () => validator(name, name.value === "" ? true : false));
+email.addEventListener("input", () => {
+  validator(
+    email, email.value === "" ? true : !validateEmail(email.value),
+    email.value === "" ? "El campo no puede estar vacío" : "El formato de correo es incorrecto");
+});
+phone.addEventListener("input", () => {
+  validator(
+    phone, phone.value === "" ? true : !validatePhone(phone.value),
+    phone.value === "" ? "El campo no puede estar vacío" : "El formato de teléfono es incorrecto");
+});
+userName.addEventListener("input", () => validator(userName, userName.value === "" ? true : false));
+password.addEventListener("input", () => {
+  validator(
+    password, password.value === "" ? true : password.value.length < 8,
+    password.value === "" ? "El campo no puede estar vacío" : "El password debe tener 8 o mas caracteres");
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Si todo es válido, mostrar datos
+  if (
+    form.checkValidity() &&
+    name.value &&
+    email.value &&
+    validateEmail(email.value) &&
+    phone.value &&
+    userName.value &&
+    password.value
+  ) {
+    getValue();
+    clearInputs()
+  }
+});
+
+
+closeBtn.addEventListener('click', () => {
+  modal.close()
+});
