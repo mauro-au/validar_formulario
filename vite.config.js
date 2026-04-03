b380ccd630846bd8f3fa525d740b7638483b2c62
mauro-au <mauricio.a.developer@gmail.com>
2026-04-03 01:26:28 -0300

    feat: se agrega pluging de vite para comprimir imagenes
    ademas se mueven las imagenes principales de assets a public

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    ViteImageOptimizer({
+      includePublic: true,
+      jpg:  { quality: 90 },
+      png:  { quality: 90 },
+      svg: {
+        plugins: [
+          { name: 'removeViewBox', active: false },
+          { name: 'removeEmptyAttrs', active: true },
+        ],
+      },
+    }),
+  ],
 })
