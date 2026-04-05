aaa4c99ad763c3f301e025576192bf0bff12f457
mauro-au <mauricio.a.developer@gmail.com>
2026-04-05 12:23:23 -0400

    feat: Se agrega compresor de imágenes para bajar su peso
    Se comprimen las imágenes sin perder calidad automáticamente al hacer el build
    Además se vuelven a dejar las imágenes en assets y no en public

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   base: './',
   plugins: [
     ViteImageOptimizer({
-      includePublic: true,
       jpg:  { quality: 90 },
       png:  { quality: 90 },
       svg: {
